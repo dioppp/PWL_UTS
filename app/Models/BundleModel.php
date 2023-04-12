29f4b879b4b626bbb913e5095a679425f5135b0e
Fikril H Ramadhani <fikril099@gmail.com>
2023-04-12 22:39:13 +0700

    try syncing

--- app/Models/BundleModel.php
+++ app/Models/BundleModel.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Bundle extends Model
+class BundleModel extends Model
 {
     use HasFactory;
+    protected $table = 'bundles';
+    protected $fillable = [
+        'name',
+        'price'
+    ];
 }
