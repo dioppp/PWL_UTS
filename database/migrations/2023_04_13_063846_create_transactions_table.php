77150a71ce916565ff72505ea71c19a0caa07b1f
Fikril H Ramadhani <fikril099@gmail.com>
2023-04-13 14:06:17 +0700

    migrations

---
+++ database/migrations/2023_04_13_063846_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cust_id');
+            $table->foreign('cust_id')->references('id')->on('customers');
+            $table->unsignedBigInteger('shoe_id');
+            $table->foreign('shoe_id')->references('id')->on('shoes');
+            $table->unsignedBigInteger('bund_id');
+            $table->foreign('bund_id')->references('id')->on('bundles');
+            $table->enum('delivery', ['Yes', 'No'])->default('Yes');
+            $table->dateTime('ordered_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->enum('status', ['pending', 'on process', 'done'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
